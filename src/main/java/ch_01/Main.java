0aa31e852a341af2d0962015186f1d253335d143
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-04 12:18:51 +0300

    add counters

--- src/main/java/ch_01/Main.java
+++ src/main/java/ch_01/Main.java
@@ -37,6 +37,7 @@ public class Main {
         for (int i = 0; i < LEN; ++i) {
             if (arr[i] == x) {
                 System.out.println("Ідекс шукаємого числа: " + i);
+                System.out.println("Кількість кроків для пошуку: " + (i + 1));
                 return;
             }
         }
@@ -44,17 +45,26 @@ public class Main {
     }
 
     private static void searchBinary(final int x, final int[] arr) {
-        int res = searchBinary(x, arr, 0, arr.length - 1);
+        MyCounter step = new MyCounter();
+        int res = searchBinary(x, arr, 0, arr.length - 1, step);
         System.out.println(res >= 0 ? "Ідекс шукаємого числа: " + res : "Шукаємого числа немає");
+        System.out.println("Кількість кроків для пошуку: " + step.get());
     }
 
-    private static int searchBinary(final int x, final int[] arr, final int from, final int to) {
+    private static int searchBinary(final int x, final int[] arr, final int from, final int to, MyCounter step) {
+        step.increment();
         if (from >= to) return -1;
         final int mid = from + (to - from) / 2;
 
         if (arr[mid] == x) return mid;
-        else if (arr[mid] < x) return searchBinary(x, arr , mid + 1, to);
-        else if (arr[mid] > x) return searchBinary(x, arr, from, mid - 1);
+        else if (arr[mid] < x) return searchBinary(x, arr , mid + 1, to, step);
+        else if (arr[mid] > x) return searchBinary(x, arr, from, mid - 1, step);
         else return -1;
+    }
+
+    private static class MyCounter {
+        private int amount = 0;
+        int get() { return amount; }
+        void increment() { ++amount; }
     }
 }
