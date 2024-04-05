f99881da89c391bfad5d9050ec0cee4728525ffe
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-05 13:13:33 +0300

    add client's code for deep walk

---
+++ src/main/java/ch_02/Main.java
@@ -0,0 +1,28 @@
+package ch_02;
+
+public class Main {
+    public static void main(String[] args) {
+        Node tree = Node.of('a',
+                Node.of('b',
+                        Node.of('d',
+                                Node.of('h', Node.of('p'), Node.of('q')),
+                                Node.of('i', Node.of('r'), Node.of('s'))
+                        ),
+                        Node.of('e',
+                                Node.of('j', Node.of('t'), Node.of('u')),
+                                Node.of('k', Node.of('v'), Node.of('w'))
+                        )
+                ), Node.of('c',
+                        Node.of('f',
+                                Node.of('l',Node.of('x'), Node.of('y')),
+                                Node.of('m', Node.of('z' ), null)
+                        ),
+                        Node.of('g', Node.of('n' ), Node.of('o'))
+                )
+        );
+
+        System.out.print("Deep walk: ");
+        tree.deepWalk();
+        System.out.println();
+    }
+}
