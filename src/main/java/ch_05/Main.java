62914fa4127b14d6a987e4c78204946dbade4839
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-11 16:43:42 +0300

    add client's code in ch_06

---
+++ src/main/java/ch_05/Main.java
@@ -0,0 +1,19 @@
+package ch_05;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CoDec codec = new CoDec();
+        User user = new User();
+        user.savePublicKey(codec.generateKeys());
+
+        System.out.print("Текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        List<String> encrypted = user.encrypt(line);
+        String decripted = codec.decript(encrypted);
+        System.out.println(decripted);
+    }
+}
